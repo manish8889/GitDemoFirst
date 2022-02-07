f913131f68af99ebb0f1907c2040ff4a3a9d95c2
Manish Kumar <m.manishec@gamil.com>
2022-02-07 12:02:13 +0530

    kotlin first commit

--- app/src/main/java/com/nic/gitdemofirst/MainActivity.kt
+++ app/src/main/java/com/nic/gitdemofirst/MainActivity.kt
@@ -2,10 +2,36 @@ package com.nic.gitdemofirst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var text: TextView;
+    lateinit var button: Button;
+    lateinit var editText: EditText;
+    lateinit var cleartext: TextView;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text = findViewById(R.id.textView4);
+        cleartext = findViewById(R.id.textView5);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        cleartext.setOnClickListener() {
+            editText.setText("");
+            text.setText("");
+        }
+
+        button.setOnClickListener() {
+            text.setText(editText.text.toString());
+            Toast.makeText(applicationContext, "executed after change made over EditText", Toast.LENGTH_SHORT).show()
+
+        }
+
+
     }
 }
